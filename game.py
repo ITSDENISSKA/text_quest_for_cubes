87ba948e5a8e3efbd26b0351f588c115450b729c
itsdenisska <fotiev.d1991@gmail.com>
2023-02-19 23:16:20 +0300

    final exe

--- game.py
+++ game.py
@@ -22,14 +22,14 @@ class Game:
         if self.room_number in room_with_objects and rooms_objects[
             room_names[self.room_number]] not in self.player.get_inventory():
             print(messages[rooms_objects[room_names[self.room_number]]])
-            print(messages["item_question"] + rooms_objects[room_names[self.room_number]] + "? (Да/Нет)", "light_green")
+            print(messages["item_question"] + rooms_objects[room_names[self.room_number]] + "? (Да/Нет)")
             answer = input().strip().lower()
             while answer not in ["да", "нет"]:
-                print(messages["error"], "red")
+                print(messages["error"])
                 answer = input().strip().lower()
             if answer == "да":
                 self.set_item_to_player()
-        print(messages["question"], "light_blue")
+        print(messages["question"])
         if "фонарик" in self.player.get_inventory():
             possible_rooms = room_connections[self.room_number]
         else:
@@ -45,34 +45,34 @@ class Game:
     def print_possible_rooms(self, possible_rooms):
         possible_variants = list()
         for number, room in enumerate(possible_rooms):
-            print(f"{number + 1}. {room_names[room]}", "cyan")
+            print(f"{number + 1}. {room_names[room]}")
             possible_variants.append(number + 1)
         choice = input()
         while not choice.isdigit() or int(choice) not in possible_variants:
-            print(messages["error"], "red")
+            print(messages["error"])
             choice = input()
         return choice
 
     # метод отвечающий за добавление вещи в инвентарь игрока
     def set_item_to_player(self):
         if not self.player.set_item(rooms_objects[room_names[self.room_number]]):
-            print(messages["inventory_error"] + self.player.get_inventory()[-1] + "? (Да/Нет)", "light_green")
+            print(messages["inventory_error"] + self.player.get_inventory()[-1] + "? (Да/Нет)")
             answer = input().strip().lower()
             while answer not in ["да", "нет"]:
-                print(messages["error"], "red")
+                print(messages["error"])
                 answer = input().strip().lower()
             if answer == "да":
                 self.player.get_inventory()[-1] = rooms_objects[room_names[self.room_number]]
-        print(messages["update_inventory"] + ", ".join(self.player.get_inventory()), "light_blue")
+        print(messages["update_inventory"] + ", ".join(self.player.get_inventory()))
 
     # метод отвечающий за битву с финальным боссом
     def fight(self, stage):
         stage_name = stages[stage]
-        print(fight_messages[f"{stage_name}_hit"], "light_green")
+        print(fight_messages[f"{stage_name}_hit"])
         answer = input().strip().lower()
         while (answer not in ["влево", "вправо"] and stage in [1, 3]) and (
                 answer not in ["да", "нет"] and stage in [2]):
-            print(messages["error"], "red")
+            print(messages["error"])
             answer = input().strip().lower()
         if stage != 4:
             if answer == "влево" or answer == "нет":
