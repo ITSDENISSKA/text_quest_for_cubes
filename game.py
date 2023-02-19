f888beef9e153dd0047569c20a70a4f5b9ed54d4
itsdenisska <fotiev.d1991@gmail.com>
2023-02-19 22:58:37 +0300

    final

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import sys
+from termcolor import cprint
 
 from player import Player
 from settings import *
@@ -9,7 +10,7 @@ from messages import *
 class Game:
     def __init__(self):
         self.room_number = 0
-        self.player =  Player()
+        self.player = Player()
         print(messages["start_messages"])
 
     # метод отвечающий за начало игры
@@ -22,14 +23,14 @@ class Game:
         if self.room_number in room_with_objects and rooms_objects[
             room_names[self.room_number]] not in self.player.get_inventory():
             print(messages[rooms_objects[room_names[self.room_number]]])
-            print(messages["item_question"] + rooms_objects[room_names[self.room_number]] + "? (Да/Нет)")
+            cprint(messages["item_question"] + rooms_objects[room_names[self.room_number]] + "? (Да/Нет)", "light_green")
             answer = input().strip().lower()
             while answer not in ["да", "нет"]:
-                print(messages["error"])
+                cprint(messages["error"], "red")
                 answer = input().strip().lower()
             if answer == "да":
                 self.set_item_to_player()
-        print(messages["question"])
+        cprint(messages["question"], "light_blue")
         if "фонарик" in self.player.get_inventory():
             possible_rooms = room_connections[self.room_number]
         else:
@@ -45,33 +46,34 @@ class Game:
     def print_possible_rooms(self, possible_rooms):
         possible_variants = list()
         for number, room in enumerate(possible_rooms):
-            print(f"{number + 1}. {room_names[room]}")
+            cprint(f"{number + 1}. {room_names[room]}", "cyan")
             possible_variants.append(number + 1)
         choice = input()
         while not choice.isdigit() or int(choice) not in possible_variants:
-            print(messages["error"])
+            cprint(messages["error"], "red")
             choice = input()
         return choice
 
     # метод отвечающий за добавление вещи в инвентарь игрока
     def set_item_to_player(self):
         if not self.player.set_item(rooms_objects[room_names[self.room_number]]):
-            print(messages["inventory_error"] + self.player.get_inventory()[-1] + "? (Да/Нет)")
+            cprint(messages["inventory_error"] + self.player.get_inventory()[-1] + "? (Да/Нет)", "light_green")
             answer = input().strip().lower()
             while answer not in ["да", "нет"]:
-                print(messages["error"])
+                cprint(messages["error"], "red")
                 answer = input().strip().lower()
             if answer == "да":
                 self.player.get_inventory()[-1] = rooms_objects[room_names[self.room_number]]
-        print(messages["update_inventory"] + ", ".join(self.player.get_inventory()))
+        cprint(messages["update_inventory"] + ", ".join(self.player.get_inventory()), "light_blue")
 
     # метод отвечающий за битву с финальным боссом
     def fight(self, stage):
         stage_name = stages[stage]
-        print(fight_messages[f"{stage_name}_hit"])
+        cprint(fight_messages[f"{stage_name}_hit"], "light_green")
         answer = input().strip().lower()
-        while (answer not in ["влево", "вправо"] and stage in [1, 3]) and (answer not in ["да", "нет"] and stage in [2]):
-            print(messages["error"])
+        while (answer not in ["влево", "вправо"] and stage in [1, 3]) and (
+                answer not in ["да", "нет"] and stage in [2]):
+            cprint(messages["error"], "red")
             answer = input().strip().lower()
         if stage != 4:
             if answer == "влево" or answer == "нет":
@@ -93,6 +95,7 @@ class Game:
                         print(endings["ending_no_with_kit"])
             else:
                 print(endings["ending_with_nothing"])
+            input()
             sys.exit(0)
 
     # метод отвечающий за конец игры
@@ -101,4 +104,3 @@ class Game:
             print(endings["ending_without_fight"])
         else:
             self.fight(1)
-
